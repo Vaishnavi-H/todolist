0924a2501ff63c77862674e75f057f6816176525
vaishnavi H <vaishnavi@accuknox.com>
2021-06-21 18:35:29 +0530

    todo forms

--- src/Components/TodoApp.js
+++ src/Components/TodoApp.js
@@ -7,7 +7,8 @@ function TodoApp(){
     const[task,setTask] = useLocalStorage("task",[]);
     const [tasklist, setTaskList] = useLocalStorage("tasklist",[]);
     const time =Date().toString();
-    
+    const[date,setDate]=useLocalStorage("date",[]);
+    const[datelist,setDateList]=useLocalStorage("",[]);
     //const initialFormData = Object.freeze({
       // text : "",
        // date: ""
@@ -24,47 +25,75 @@ function TodoApp(){
 //  };
    // }
 
-   const handleChange = (e)=>{
+
+   const TexthandleChange = (e)=>{
        setTask(e.target.value);
    };
-      
-      const AddTask = () => {
-        if(task !== ""){
+   const DatehandleChange =(e)=>{
+       setDate(e.target.value);
+   };
+   
+      const AddTask =() => {
+        if(task  !== ""){
             const taskDetails={
                 id: Math.floor(Math.random()*1000),
                 value: task,
-                date:time,
                 
-                
-           };
+         };
+        
+    
         setTaskList([...tasklist, taskDetails]);
         }
     };
+
+    const AddDate =() => {
+        if(task  !== ""){
+            const dateDetails={
+               
+               
+                value: date,
+         };
+        
+    
+        setDateList([...datelist, dateDetails]);
+        }
+    };
+
+
+    
+
  const removeTask = (e, id )=>{
         e.preventDefault();
         setTaskList(tasklist.filter((t) => t.id !== id));
 };
 
+
 return(
     <div>
         <h3>Todolist</h3>
-        {tasklist !== [] ? (
         <div>
-             {tasklist.map((t,time) => (
-             <div class="ui"> {t.value}{t.time} 
-            
-            <button onClick={(e)=> removeTask(e, t.id)}><i class="fa fa-close"></i></button></div>))}
-         </div>        
+             {tasklist.map((t) => (
+             <li class="ui"> {t.value}
+               <button onClick={(e)=> removeTask(e, t.id)}><i class="fa fa-close"></i></button>
+             </li>))}
+             {datelist.map((a) => (<li class="vi">{a.value}<button onClick={(e)=> removeTask(e, a.id)}><i class="fa fa-close"></i></button></li> 
+                   ))}
+                  
+                  </div>  
+                 
+           
         
-        ): null }
+       
+        
+        
         <form >
-        <input type="text" placeholder="Enter Item" id="text" name="text" onChange={(e) => handleChange(e)} />
-        <input type="date" name="date" onChange={(e) => handleChange(e)}/>
+        <input type="text" placeholder="Enter Item" id="text" name="text" onChange={(e) => TexthandleChange(e)} />
+        <input type="date" name="date" onChange={(e) => DatehandleChange(e)}/>
         
-        <button class="btn" onClick={AddTask}>Submit</button>
+        <button class="btn" type="submit" onClick={AddTask} >Submit</button>
         
         </form>
-        <br></br>
+        
         
     </div>
       );
